c3a7a530d44c788679444aaa3b8a842400ac54a8
abhishek@srichidtechnologies.com <abhishek@srichidtechnologies.com>
2020-06-29 08:42:04 +0530

    bootstrap, angular pipes and conditional content display

--- src/app/customer.ts
+++ src/app/customer.ts
@@ -16,3 +16,43 @@ export class Customer{
 // </table>
   
 // customers:Customer[]=[{firstName:"2",lastName:"5"}];
+
+
+// <div class="container"> <h1 class="mt-3 mb-3">Products</h1> 
+//     <table class="table table-hover">
+//         <thead>
+//         <tr>
+//             <th>
+//     Product Name
+//             </th>
+//             <th>
+//               Category  
+//             </th>
+//             <th>
+//                 Description
+//             </th>
+//             <th>
+//                 Price(Rs.)
+//             </th>
+//             <th>
+//              Availability
+//             </th>
+//         </tr>
+//     </thead>
+//     <tbody>
+//         <tr *ngFor="let product of products">
+//             <td>{{product.ProductName}}</td>
+//             <td>{{product.Category}}</td>
+//             <td>{{product.Description}}</td>
+//             <td>{{product.Price}}</td>
+//             <td>
+//                 <span *ngIf="product.Availability">YES</span>
+//                 <span *ngIf="!product.Availability">NO</span>
+//                 <!-- {{product.Availability}} -->
+//             </td>
+//         </tr>
+//     </tbody>
+    
+    
+//     </table>
+// </div>
